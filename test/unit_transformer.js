98f21b32048de2f15e390d48566227dd7776c894
Steven Looman <steven.looman@gmail.com>
2012-01-09 11:22:59 +0100

    Implemented UnitTransformer/AddOperation

--- test/unit_transformer.js
+++ test/unit_transformer.js
@@ -2,7 +2,7 @@ var mco = require('..');
 
 
 module.exports = {
-    test_UnitTransformer_apply: function(beforeExit, assert) {
+    test_UnitTransformer_apply_MultiplyOperation: function(beforeExit, assert) {
         var metre = new mco.BaseUnit('m');
         var feet = new mco.BaseUnit('ft');
         var metreToFeet = new mco.UnitTransformer(
@@ -13,5 +13,18 @@ module.exports = {
         var lengthFeet = metreToFeet.apply(lengthMetre);
         assert.eql(32.808399, lengthFeet.getValue());
         assert.eql('ft', lengthFeet.getUnit().getSymbol());
+    },
+
+    test_UnitTransformer_apply_AddOperation: function(beforeExit, assert) {
+        var kelvin = new mco.BaseUnit('K');
+        var celcius = new mco.BaseUnit('°C');
+        var kelvinToCelcius = new mco.UnitTransformer(
+            kelvin, celcius, [ new mco.AddOperation(-273.15) ]
+        );
+        var temperatureKelvin = new mco.UnitValue(273.15, kelvin);
+
+        var temperatureCelcius = kelvinToCelcius.apply(temperatureKelvin);
+        assert.eql(0, temperatureCelcius.getValue());
+        assert.eql('°C', temperatureCelcius.getUnit().getSymbol());
     },
 };
