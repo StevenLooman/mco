93babc4ae4acb68d43d2c853ed58b457af0c2b7a
Steven Looman <steven.looman@gmail.com>
2012-08-21 23:03:56 +0200

    StringSerializer

---
+++ test/string_serializer.js
@@ -0,0 +1,51 @@
+var mco = require('..');
+var assert = require('assert');
+
+describe('StringSerializer', function() {
+    describe('#registerUnit()', function() {
+        it('should be able to register a valid BaseUnit', function() {
+            var metre = new mco.BaseUnit(mco.Quantity.LENGTH, 'm');
+            var uv = new mco.UnitValue(25, metre);
+
+            var ss = new mco.StringSerializer();
+            ss.registerUnit(metre);
+        });
+    });
+
+    describe('#parseUnit()',  function() {
+        it('should be able to parse a registered Unit', function() {
+            var metre = new mco.BaseUnit(mco.Quantity.LENGTH, 'm');
+            var ss = new mco.StringSerializer();
+            ss.registerUnit(metre);
+
+            var r = ss.parseUnit('[m]');
+
+            assert.deepEqual(r, metre);
+        });
+    });
+
+    describe('#serialize()',  function() {
+        it('should be able to serialize a UnitValue to a string', function() {
+            var metre = new mco.BaseUnit(mco.Quantity.LENGTH, 'm');
+            var uv = new mco.UnitValue(25, metre);
+            var ss = new mco.StringSerializer();
+
+            var str = ss.serialize(uv);
+
+            assert.equal(str, "25 [m]");
+        });
+    });
+
+    describe('#deserialize()',  function() {
+        it('should be able to deserialize a string to a UnitValue', function() {
+            var metre = new mco.BaseUnit(mco.Quantity.LENGTH, 'm');
+            var uv = new mco.UnitValue(25, metre);
+
+            var ss = new mco.StringSerializer();
+            ss.registerUnit(metre);
+            var r = ss.deserialize('25 [m]');
+
+            assert.deepEqual(r, uv);
+        });
+    });
+});
